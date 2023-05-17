dd7bae7eb9ebdc14dc1310060ecc10c5e21171da
Camilo Figueroa <cigotete@gmail.com>
2023-05-17 17:30:59 -0500

    Brand CRUD service methods, and respectives DTOs and entity.

--- src/brands/brands.service.ts
+++ src/brands/brands.service.ts
@@ -1,26 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Brand } from './entities/brand.entity';
 import { CreateBrandDto } from './dto/create-brand.dto';
 import { UpdateBrandDto } from './dto/update-brand.dto';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class BrandsService {
+  private brands: Brand[] = [
+    { id: uuid(), name: 'Audi', createdAt: Date.now() },
+  ];
+
   create(createBrandDto: CreateBrandDto) {
-    return 'This action adds a new brand';
+    const newBrand: Brand = {
+      id: uuid(),
+      name: createBrandDto.name.toLocaleLowerCase(),
+      createdAt: Date.now(),
+    };
+    this.brands.push(newBrand);
+    return newBrand;
   }
 
   findAll() {
-    return `This action returns all brands`;
+    return this.brands;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} brand`;
+  findOne(id: string) {
+    const itemWithId = this.brands.find((item) => item.id === id);
+    if (!itemWithId) {
+      throw new NotFoundException(`Item with ID ${id} not found`);
+    }
+    return itemWithId;
   }
 
-  update(id: number, updateBrandDto: UpdateBrandDto) {
-    return `This action updates a #${id} brand`;
+  update(id: string, updateBrandDto: UpdateBrandDto) {
+    let brandWithId = this.findOne(id);
+
+    this.brands = this.brands.map((item) => {
+      if (item.id === id) {
+        brandWithId = {
+          ...brandWithId,
+          ...updateBrandDto,
+          id,
+          updatedAt: Date.now(),
+        };
+        return brandWithId;
+      }
+      return item;
+    });
+    return brandWithId;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} brand`;
+  remove(id: string) {
+    this.brands = this.brands.filter((item) => item.id !== id);
+    return 'Item deleted';
   }
 }
--- src/brands/dto/update-brand.dto.ts
+++ src/brands/dto/update-brand.dto.ts
@@ -1,4 +1,7 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateBrandDto } from './create-brand.dto';
+import { IsOptional, IsString, IsUUID, MinLength } from 'class-validator';
 
-export class UpdateBrandDto extends PartialType(CreateBrandDto) {}
+export class UpdateBrandDto {
+  @IsString()
+  @MinLength(1)
+  readonly name: string;
+}
